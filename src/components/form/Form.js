c01c3767e0b06223195a55ef26da424e6f356fd9
Kevin <kevin.tassi@gmail.com>
2019-10-17 14:50:51 +0200

    Create class base component

---
+++ src/components/form/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Form extends React.Component {
+
+  state = {
+    name: '',
+    quantity: 0
+  }
+
+  render() {
+    return (<div>Form</div>)
+  }
+}
+
+export default Form
