51f1b229c1441e86f38b2a84f5edd539ebda7c86
Kevin Tassi <kevin@wooclap.com>
2020-02-13 15:34:43 +0100

    Updates

---
+++ precache-manifest.deaa5c900189b1bf1c8c40a06f49850d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f72c01b96893f843946fa5b94f9e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f43210295e1de0d2c9f",
+    "url": "/static/css/main.c7d84c77.chunk.css"
+  },
+  {
+    "revision": "df2531f694ac312d1049",
+    "url": "/static/js/2.df5795b2.chunk.js"
+  },
+  {
+    "revision": "4f43210295e1de0d2c9f",
+    "url": "/static/js/main.0acd005b.chunk.js"
+  },
+  {
+    "revision": "b25ad653faebefd798c5",
+    "url": "/static/js/runtime-main.68ea0707.js"
+  }
+]);
